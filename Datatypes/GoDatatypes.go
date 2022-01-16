3b0021f5b13ac531b207b02fd5d11dfca94d36e3
root <adityadeshmukh7350@gmail.com>
2022-01-16 08:29:19 -0800

    init

---
+++ Datatypes/GoDatatypes.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	/*A type determines a set of values together with operations and methods specific to those values
+	there are three types:
+		1.predeclared types
+		2.introduced types
+		3.comosite types
+	Predeclared/Build in types
+	* Numeric types
+		-- int8,int16,int32,int64
+		-- uint8,uint16,uint32,uint64:Used to represent unsigned (positive) integer.
+		--float32,float64: zero before the decimal point seperator can be omitted */
+
+}
