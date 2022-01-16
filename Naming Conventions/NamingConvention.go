5cc901ce71a9b8b12f5a1601848983be0792f9b4
root <adityadeshmukh7350@gmail.com>
2022-01-16 06:40:21 -0800

    init

---
+++ Naming Conventions/NamingConvention.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	//Naming Convenstion
+	//-Name Start with a letter or an underscore(_)
+	//-Case Matters: quickSort and quickSort are different
+	//-Go keyword(25) can not be used as names
+	//-Use the first letters of the words
+	//How to declear variable
+	//var fn int //fn -> First Number
+	//-Use fewer letters in smaller scopes and the complete word in large scopes
+
+}
---
+++ fmt package/FmtPackage.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	//whats is fmt?
+	//fmt is go standard libray package
+	//function in fmt package
+	//1.Print
+	//2.Println
+	//3.Println
+	//4.Sprint
+	//and many more
+	//Use fmt package
+	//1.Println
+	fmt.Println("Fmt Package")
+	name := "Aditya"
+	fmt.Println("Hello My name is", name)
+	//here name is argument of Println funcyion
+	//2.Printf
+	fmt.Printf("My age is %d\n", 21)
+	fmt.Printf("x is %d and y is %f", 2, 55.55)
+}
