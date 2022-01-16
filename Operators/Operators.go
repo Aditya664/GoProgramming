1cf3510e71bdca81ea77f25b4913f35b3d293f80
root <adityadeshmukh7350@gmail.com>
2022-01-16 09:46:09 -0800

    init

---
+++ Operators/Operators.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	//What is an Operator?
+	/*An Operator is a symboll of the programming language whichis able to
+	operate on values.in GO languange Operators can
+	be categorized based upon their different functionallity
+	is these category:
+		* Arithmatic operator and bitwise operator: +,-,*,/,&,|,^,<,>.
+		* Assignment Operator: +=,-=,*=,/=,%=.
+		* Increament and decrement: ++,--.
+		* Comparison operator: ==,!=,<=,>=,<,>.
+		* Logical Operator: &&,||.
+		* Operators for Pointer(&) and channels(<-).*/
+}
