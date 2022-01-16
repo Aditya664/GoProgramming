ccec50876f45a54626ca03f9f4abb822c874522e
root <adityadeshmukh7350@gmail.com>
2022-01-16 08:41:51 -0800

    init

--- Datatypes/GoDatatypes.go
+++ Datatypes/GoDatatypes.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	/*A type determines a set of values together with operations and methods specific to those values
 	there are three types:
@@ -8,8 +10,22 @@ func main() {
 		3.comosite types
 	Predeclared/Build in types
 	* Numeric types
-		-- int8,int16,int32,int64
+		-- int8,int16,int32,int64:Used to represent signed integer
 		-- uint8,uint16,uint32,uint64:Used to represent unsigned (positive) integer.
-		--float32,float64: zero before the decimal point seperator can be omitted */
+		--float32,float64: zero before the decimal point seperator can be omitted
+		--byte(alias for uint8)
+		--*/
+
+	//code
+	//int8
+	var i1 int8 = -128
+	fmt.Printf("%T\n", i1)
+	//int16
+	//var i2 int16 = 65539
+	//fmt.Printf("%T\n", i2)
+
+	//float64
+	var f1, f2, f3 float64 = 1.1, -20.0, 5.0
+	fmt.Printf("%T %T %T", f1, f2, f3)
 
 }
