cb667eb07aa3290f694226d72082fc3818c459eb
root <adityadeshmukh7350@gmail.com>
2022-01-16 10:02:44 -0800

    init

--- Operators/Operators.go
+++ Operators/Operators.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	//What is an Operator?
 	/*An Operator is a symboll of the programming language whichis able to
@@ -11,5 +13,21 @@ func main() {
 		* Increament and decrement: ++,--.
 		* Comparison operator: ==,!=,<=,>=,<,>.
 		* Logical Operator: &&,||.
-		* Operators for Pointer(&) and channels(<-).*/
+		* Operators for Pointer(&) and channels(<-).
+	# Arithmatic operator
+	it is used when we need to perform some common maths operations.
+	there are following arithmatic operator:
+		+(addition)
+		-(substration)
+		*(multiplication)
+		/(Division)
+		%(Modulus)
+	*/
+	//code
+	var a int = 55
+	var b int = 5
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
 }
